b41b177647f52f12d2e5086be14987093453faf9
imran-shaik-927 <imranshaik927927@gmail.com>
2023-09-19 16:38:25 +0530

    updated security groups

--- aws-vpccomponents.tf
+++ aws-vpccomponents.tf
@@ -117,3 +117,72 @@ resource "aws_network_acl" "ecomm-pvt-nacl" {
     Name = "ecomm-private-nacl"
   }
 }
+#Public security group
+resource "aws_security_group" "ecomm-pub-sg" {
+  name        = "ecomm-web"
+  description = "Allow shh & HTTPS inbound traffic"
+  vpc_id      = aws_vpc.imranvpc.id
+
+  ingress {
+    description      = "ssh from WWW"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+ingress {
+    description      = "HTTP from WWW"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-public-firewall"
+  }
+}
+
+#Private security group
+resource "aws_security_group" "ecomm-pvt-sg" {
+  name        = "ecomm-db"
+  description = "Allow shh & mysql inbound traffic"
+  vpc_id      = aws_vpc.imranvpc.id
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+
+  }
+ingress {
+    description      = "mysql  from db"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-db-firewall"
+  }
+}
